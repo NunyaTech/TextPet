1996a4a4b1072f460890fe3fb095a21acfea29ee
NunyaTech <aahanstudios@gmail.com>
2026-08-18 14:16:27 -0400

    Refactor textPet.py for better readability and structure

--- textPet.py
+++ textPet.py
@@ -3,53 +3,72 @@ pet = input("""welcome to text pet, a virtual pet played with only text!
 In this pet simulator you have to take care  of a virtual pet of your choice!
 Which pet would you like? a) dog, b) cat, c) bird, d)hamster (input the letter): """)
 if pet == "a" or "dog":
-    while True:
-        what_to_do = random.randint(1, 3)
         print("Congrats on your new dog!")
-        dogName = input("Name Him: ")
+        Name = input("Name Him: ")
         while True:
+            what_to_do = random.randint(1, 3)
             if what_to_do == 1:
-                dogFood = input(f"Ruff Ruff! {dogName} is hungry! type f to feed him!")
-                if dogFood == "f":
-                    print(f"Ruff Ruff! Pant! {dogName} is Happy!")
+                Food = input(f"Ruff Ruff! {Name} is hungry! type f to feed him!")
+                if Food == "f":
+                    print(f"Ruff Ruff! Pant! {Name} is Happy!")
                 else:
                     print("Really, you're not feeding your own pet!?")
             elif what_to_do == 2:
-                dogDrink = input(f"{dogName} is very thirsty. Type w to satisfy his thirst.")
-                if dogDrink == "w":
-                    print(f"Pant Pant(gulp, sigh), {dogName} is so grateful you nourished him with water!")
+                Drink = input(f"{Name} is very thirsty. Type w to satisfy his thirst.")
+                if Drink == "w":
+                    print(f"Pant Pant(gulp, sigh), {Name} is so grateful you nourished him with water!")
                 else:
-                    print(f"{dogName} is very unhappy you did not give him water.")
+                    print(f"{Name} is very unhappy you did not give him water.")
             elif what_to_do == 3:
-                dogBathroom = input(f"{dogName} needs to go to the bathroom. Type b to take him out.")
-                if dogBathroom == "b":
-                    print(f"{dogName} is very satisfied!")
+                Bathroom = input(f"{Name} needs to go to the bathroom. Type b to take him out.")
+                if Bathroom == "b":
+                    print(f"{Name} is very satisfied!")
                 else:
-                    print(f"{dogName} is very unhappy with you.")
+                    print(f"{Name} is very unhappy with you.")
 if pet == "b" or "cat":
-    while True:
         what_to_do2 = random.randint(1, 3)
         print("Congrats on your new cat!")
-        catName = input("Name Her: ")
+        Name = input("Name Her: ")
         while True:
             if what_to_do2 == 1:
-                catFood = input(f"meow! {catName} is hungry! type f to feed him!")
-                if catFood == "f":
-                    print(f"meow! {catName} is Happy!")
+                Food = input(f"meow! {Name} is hungry! type f to feed her!")
+                if Food == "f":
+                    print(f"meow! {Name} is Happy!")
                 else:
                     print("Really, you're not feeding your own pet!?")
             elif what_to_do2 == 2:
-                catDrink = input(f"{catName} is very thirsty. Type w to satisfy her thirst.")
-                if catDrink == "w":
-                    print(f"meow! {catName} is so grateful you nourished her with water!")
+                Drink = input(f"{Name} is very thirsty. Type w to satisfy her thirst.")
+                if Drink == "w":
+                    print(f"meow! {Name} is so grateful you nourished her with water!")
                 else:
-                    print(f"{catName} is very unhappy you did not give her water.")
+                    print(f"{Name} is very unhappy you did not give her water.")
             elif what_to_do2 == 3:
-                catBathroom = input(f"{catName} used the bathroom! Type c to clean her litterbox")
-                if catBathroom == "b":
-                    print(f"{catName} is very happy with her clean bathroom.")
+                Bathroom = input(f"{Name} used the bathroom! Type c to clean her litterbox")
+                if Bathroom == "b":
+                    print(f"{Name} is very happy with her clean bathroom.")
                 else:
                     print(f"Your house is stinking right now. Should have cleaned that litter.")            
-
-                
-            
+if pet == "c" or "bird":
+        what_to_do2 = random.randint(1, 3)
+        print("Congrats on your new bird!")
+        Name = input("Name Her: ")
+        while True:
+            if what_to_do2 == 1:
+                Food = input(f"chirp! {Name} is hungry! type f to feed her!")
+                if Food == "f":
+                    print(f"chirp! {Name} is Happy!")
+                else:
+                    print("Really, you're not feeding your own pet!?")
+            elif what_to_do2 == 2:
+                Drink = input(f"{Name} is very thirsty. Type w to satisfy her thirst.")
+                if Drink == "w":
+                    print(f"chirp! {Name} is so grateful you nourished her with water!")
+                else:
+                    print(f"{Name} is very unhappy you did not give her water.")
+            elif what_to_do2 == 3:
+                Bathroom = input(f"{Name} used the bathroom! Type c to clean her litterbox")
+                if Bathroom == "b":
+                    print(f"{Name} is very happy with her clean bathroom.")
+                else:
+                    print(f"Your house is stinking right now. Should have cleaned that litter.")
+                    
